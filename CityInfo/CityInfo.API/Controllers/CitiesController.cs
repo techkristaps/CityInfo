d696d8a2779d458946b8e6639b6b8d98fbfd9d0e
Kristaps Kozlovskis <228943118+techkristaps@users.noreply.github.com>
2025-09-02 21:39:35 +0300

    Added code

--- CityInfo/CityInfo.API/Controllers/CitiesController.cs
+++ CityInfo/CityInfo.API/Controllers/CitiesController.cs
@@ -1,18 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CityInfo.API.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CityInfo.API.Controllers
 {
     [ApiController]
+    [Route("api/cities")]
     public class CitiesController : ControllerBase
     {
+        [HttpGet]
         public JsonResult GetCities()
         {
-            return new JsonResult(
-                new List<object>
-                {
-                    new {id = 1, Name = "New York City"},
-                    new {id = 2, Name = "Antwerp"}
-                });
+            return new JsonResult(CitiesDataStore.Current.Cities);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<CityDto> GetCity(int id)
+        {
+            // find city
+            var cityToReturn = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == id);
+
+            return Ok(cityToReturn);
         }
     }
-}
+} 
